3d519a6e7d0c1268a2bf5d6d7ab4f8b21b81c610
opeyemipeter <opeyemi@dufuna.com>
2021-10-25 16:03:24 +0100

    express/middleware

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,8 @@
+// exports.myMiddleware = (req, res, next) => {
+//     req.name = 'yes',
+//         next()
+}
 exports.homePage = (req, res) => {
+    // console.log(req.name);
     res.render('index')
 }
