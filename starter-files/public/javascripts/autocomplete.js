893e66af637a4a3844929cb8df9a4074d6bfa322
opeyemipeter <opeyemi@dufuna.com>
2021-11-03 05:19:30 +0100

    lates

--- starter-files/public/javascripts/autocomplete.js
+++ starter-files/public/javascripts/autocomplete.js
@@ -1,6 +1,21 @@
 function autocomplete(input, latInput, lngInput) {
-    console.log(input, latInput, lngInput);
+    // console.log(input, latInput, lngInput);
+    if (!input) return;
+    const dropdown = new google.maps.places.Autocomplete(input)
+
+    dropdown.addListener('place_changed', () => {
+        const place = dropdown.getPlace();
+        console.log(place);
+        latInput.value = place.geometry.latitude.lat()
+        lngInput.value = place.geometry.longitude.lng()
+
+        input.on('keydown', (event) => {
+            if (e, keyCode === 13) {
+                e.preventDefault();
+            }
+        })
+    })
 
 }
 
-export default autocomplete;
+export default autocomplete
