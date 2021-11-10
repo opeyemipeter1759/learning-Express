84803ddd390c3e4857a3adddc8fafcd0cdca33ad
opeyemipeter <opeyemi@dufuna.com>
2021-11-10 15:42:15 +0100

    vakidator

--- starter-files/controllers/userController.js
+++ starter-files/controllers/userController.js
@@ -7,3 +7,13 @@ exports.loginForm = (req, res) => {
 exports.registerForm = (req, res) => {
     res.render("register", { title: "Register Your Account" });
 };
+exports.validateRegister = (rer, res, next) => {
+    req.sanitizeBody("name");
+    req.checkBody("name", "You must supply a name").notEmpty();
+    req.checkBody("email", "That Email is not Valid").isEmail();
+    req.sanitizeBody("email").normalizeEmail({
+        remove_dots: false,
+        remove_extension: false,
+        gmail_remove_subaddress: false,
+    });
+};
